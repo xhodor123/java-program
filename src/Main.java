1337227b63ee6a3ddec8dc69bddb82e4a1548a97
Matus Hodor <hodorm@gmail.com>
2024-11-13 11:30:02 +0100

    new featured edited

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
         System.out.println("Hello world!");
 
 
-        System.out.println("super new feature");
+        System.out.println("super new feature edited");
     }
 }
